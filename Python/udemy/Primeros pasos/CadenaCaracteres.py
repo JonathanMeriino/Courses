4658d4b20dd6ae597e806d05c5c65b1d5b18e8ef
Jon BM <jbenitez.m14@gmail.com>
2021-04-04 12:09:43 -0500

    Cadena de caracteres

---
+++ Python/udemy/Primeros pasos/CadenaCaracteres.py
@@ -0,0 +1,22 @@
+'''
+'Alvaro Chirou'
+"Hola estudiantes"
+'Alvaro saluda sus "estudiantes"'
+
+'''
+
+stre ='Alvaro Chirou'
+type(stre)
+
+print('comillas simples')
+print("Comillas dobles")
+
+print('Chirou "Hola"')
+
+print("Este texto tiene \n una nueva linea")
+
+print("Este texto tiene \nuna nueva linea")
+
+print("este texto tiene \tuna nueva linea")
+
+print(r'C:\nombres\clases')
