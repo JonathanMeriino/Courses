dab22b7f076e297d33140df4f474d1f98d31430b
Jon BM <jbenitez.m14@gmail.com>
2021-04-04 12:33:48 -0500

    listas

---
+++ Python/udemy/Primeros pasos/listas.py
@@ -0,0 +1,47 @@
+listas = [5,9,"Alvaro",-8,"estudiantes"]
+
+print(listas[0])
+
+print(listas[-1])
+
+print(listas[2])
+
+print(listas[2:])
+
+primer_parte = [1,2,3,4]
+segunda_parte = [5,6,7]
+
+print(primer_parte+segunda_parte)
+
+print(segunda_parte + [8,9,10])
+
+numeros = [1,2,3,4,9,6,7,8,9]
+print(numeros)
+numeros [4] = 5
+
+print(numeros)
+
+numeros.append(10)
+
+print(numeros)
+
+numeros.append(10+1)
+
+print(numeros)
+
+abcedario =['A','B','C','D']
+
+print(abcedario)
+
+print(abcedario[:2])
+
+abcedario[:2] = ['a','b']
+print(abcedario)
+
+primero = [1,2,3]
+segundo = [4,5,6]
+tercero = [7,8,9]
+cuarto = [10,11,12]
+anidadas = [primero,segundo,tercero,cuarto]
+
+print(anidadas[0])
