e2eabc86e016c7795876bb7a00ef8bbc016437bf
Jon BM <jbenitez.m14@gmail.com>
2021-04-04 12:16:21 -0500

    Cadena de caracteres parte 2

---
+++ Python/udemy/Primeros pasos/cadenaCaracterespt2.py
@@ -0,0 +1,15 @@
+print("""De esta manera 
+nos ahorramos a nun 
+pero no a \tuna que es para tabulacion""")
+
+variable ="Teoria sobre este tema donde salto de linea es con \n son dos lineas"
+
+
+print(variable)
+
+print(variable + variable)
+
+primero = "primer parte"
+segundo = "segunda parte"
+
+print("Fusion de "+primero+segundo)
