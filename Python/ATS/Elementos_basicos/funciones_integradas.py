f7ea1ed366767287f31807f93dfc486049bb5e72
Jon BM <jbenitez.m14@gmail.com>
2021-04-03 23:23:57 -0600

    Funciones integradas

---
+++ Python/ATS/Elementos_basicos/funciones_integradas.py
@@ -0,0 +1,33 @@
+#funciones integradas
+
+n= str(10)
+
+print(n)
+
+n= bin(10)
+
+print(n)
+
+n=hex(10)
+
+print(n)
+
+n= int("0b1010",2)
+
+print(n)
+
+n = abs(-8)
+
+print(n)
+
+n = round(5.6)
+
+print(n)
+
+n = round(5.4)
+
+print(n)
+
+n = len("Jonathan")
+
+print(n)
