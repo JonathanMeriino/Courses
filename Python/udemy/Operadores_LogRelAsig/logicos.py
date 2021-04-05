69e637d6c118c4a0c6e9753727b070e992c27d2a
Jon BM <jbenitez.m14@gmail.com>
2021-04-04 20:24:59 -0500

    Operadores logicos

---
+++ Python/udemy/Operadores_LogRelAsig/logicos.py
@@ -0,0 +1,34 @@
+
+#not
+print(not True)
+
+not False == True
+
+print(not False)
+#and
+print(False and False)
+
+print(True and True)
+
+numero = 10
+
+print(numero>5 and numero<15)
+
+print(numero>5 and numero>15)
+
+print(True and False)
+
+#or
+
+print(numero>5 or numero>15)
+
+print(True or False)
+
+print(False or False)
+
+palabra = 'Alvaro'
+
+print(palabra == "Salir" or palabra == "Terminar" or palabra == "Exit")
+print(palabra == "Salir" or palabra == "Alvaro" or palabra == "Exit")
+
+print(palabra[0]=="A")
