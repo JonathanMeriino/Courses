4b2c00fbda64cfe8526f04b1aee69f3b5c6a3cba
Jon BM <jbenitez.m14@gmail.com>
2021-04-04 20:42:05 -0500

    Operadores anidadas

---
+++ Python/udemy/Operadores_LogRelAsig/expresionesAnidadas.py
@@ -0,0 +1,32 @@
+primero = 8
+segundo = 10
+
+resultado= primero*segundo-4**primero or not (primero%segundo) == 0
+
+print(resultado)
+
+resultado= primero*segundo-4**primero <=8 or not (primero%segundo) == 0
+
+print(resultado)
+
+valor = 10
+
+valor += 10
+
+print(valor)
+
+valor -=10
+
+print(valor)
+
+valor *= 10
+print(valor)
+
+valor /=10
+print(valor)
+
+valor %=10
+print(valor)
+
+valor **=10
+print(valor)
