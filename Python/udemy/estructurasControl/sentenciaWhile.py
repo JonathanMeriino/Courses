4f5497a2a80a3122dcc69189a017f453f57318de
Jon BM <jbenitez.m14@gmail.com>
2021-04-05 14:11:44 -0500

    Sentencia while

---
+++ Python/udemy/estructurasControl/sentenciaWhile.py
@@ -0,0 +1,32 @@
+iteracion =1
+
+while iteracion <=3:
+    iteracion += 1
+    print("Estoy iterando, van = ",iteracion)
+
+iteracion =0
+
+while iteracion <=0:
+    iteracion += 1
+    print("Estoy iterando, van = ",iteracion)
+else:
+    print("Imprimo esto por que se termino y es el else")
+
+iteracion =0
+
+while iteracion <=0:
+    iteracion += 1
+    print("Estoy iterando, van = ",iteracion)
+    break
+else:
+    print("Imprimo esto por que se termino y es el else")
+
+iteracion =0
+
+while iteracion <=10:
+    iteracion += 1
+    if (iteracion==4):
+        print("Estoy iterando, van = ",iteracion)
+        break
+else:
+    print("Imprimo esto por que se termino y es el else")
