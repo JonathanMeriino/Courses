bbd890f4edfbeb91236743b92bce2519e6f7b2c5
Jon BM <jbenitez.m14@gmail.com>
2021-04-05 18:50:59 -0500

    colas

---
+++ Python/udemy/seccion6/colas.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+colas = deque()
+
+print(colas)
+
+print(type(colas))
+
+colas = deque(['Alvaro','Estudiantes','Familia','Genios'])
+
+print(colas)
+
+print(colas.pop())
+
